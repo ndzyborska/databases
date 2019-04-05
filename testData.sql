df1c337acbd0ed39c2296be19fbbadd6a5af99f2
jr15936 <jr15936@bristol.ac.uk>
2019-04-05 15:27:15 +0100

    created testData

---
+++ testData.sql
@@ -0,0 +1,4 @@
+INSERT INTO Person (name,username,stuId) VALUES ("userA", "uA123","12345");
+INSERT INTO Person (name,username,stuId) VALUES ("userB", "uB456","32432");
+INSERT INTO Person (name,username,stuId) VALUES ("userC", "uC789","99999");
+INSERT INTO Person (name,username) VALUES ("userD", "uD321");
