125125468fa1f9b1e8140e185a308493f6b38f20
jr15936 <jr15936@bristol.ac.uk>
2019-04-05 15:47:53 +0100

    added more test data

--- testData.sql
+++ testData.sql
@@ -2,3 +2,11 @@ INSERT INTO Person (name,username,stuId) VALUES ("userA", "uA123","12345");
 INSERT INTO Person (name,username,stuId) VALUES ("userB", "uB456","32432");
 INSERT INTO Person (name,username,stuId) VALUES ("userC", "uC789","99999");
 INSERT INTO Person (name,username) VALUES ("userD", "uD321");
+
+INSERT INTO Forum (name) VALUES ("databases");
+
+INSERT INTO Topic (title,message,forumId,personId) VALUES ("test topic","test message",1,1);
+INSERT INTO Topic (title,message,forumId,personId) VALUES ("test topic 2","test message 2",1,2);
+
+INSERT INTO Post (message,topicId,personId) VALUES ("test post in topic",1,3);
+INSERT INTO Post (message,topicId,personId) VALUES ("test post in topic 2",2,4); 
